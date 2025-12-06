4ebe5f27895eab3d339ea8e74390732181981f65
noobieyuh <156363695+noobieyuh@users.noreply.github.com>
2025-12-06 01:03:06 -0600

    chore: fix build

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ const createWindow = () => {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
+            preload: path.join(__dirname, 'src/preload.js'),
         },
     })
 
-    win.loadFile('index.html')
+    win.loadFile('src/index.html')
 }
 
 app.whenReady().then(() => {
