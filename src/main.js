ae6089eb0f67528787bbb4474d34e4040718f56e
noobieyuh <156363695+noobieyuh@users.noreply.github.com>
2025-12-07 22:47:24 -0600

    change window size

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ const path = require('node:path')
 const createWindow = () => {
     const win = new BrowserWindow({
         autoHideMenuBar: true,
-        width: 800,
-        height: 600,
+        width: 1600,
+        height: 1200,
         webPreferences: {
             preload: path.join(__dirname, 'src/preload.js'),
         },
