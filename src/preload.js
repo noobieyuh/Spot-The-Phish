8ad481bf53d6b5755d6b6a2183124615beb431e5
noobieyuh <156363695+noobieyuh@users.noreply.github.com>
2025-12-16 00:30:32 -0600

    testing vite

--- src/preload.js
+++
@@ -1,8 +0,0 @@
-// From the JellyPhishers
-const { contextBridge } = require('electron/renderer')
-
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-})
