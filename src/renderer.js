07dfe4f7245f727755c9c5ccb13dfa747b443710
noobieyuh <156363695+noobieyuh@users.noreply.github.com>
2025-12-16 01:04:18 -0600

    its 1am and i need to change something

--- src/renderer.js
+++
@@ -1,5 +0,0 @@
-// From the JellyPhishers
-import React from 'react'
-
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
